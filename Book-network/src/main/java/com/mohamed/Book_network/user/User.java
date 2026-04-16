dea9f0960b9b84f90b85b091fa65abba551639ac
MO Othman <othman9128@gmail.com>
2026-04-16 17:29:55 +0200

    impl the User Class

--- Book-network/src/main/java/com/mohamed/Book_network/user/User.java
+++ Book-network/src/main/java/com/mohamed/Book_network/user/User.java
@@ -1,4 +1,91 @@
 package com.mohamed.Book_network.user;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "_user")
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
+
+    @GeneratedValue
+    @Id
+    private Integer id ;
+    private String f_name;
+    private String l_name ;
+    private LocalDate dataOfBirth;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    private boolean enabled;
+    private boolean accountLocked;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdData ;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedData;
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    private String getFullName(){
+        return f_name + " " + l_name ;
+    }
 }
