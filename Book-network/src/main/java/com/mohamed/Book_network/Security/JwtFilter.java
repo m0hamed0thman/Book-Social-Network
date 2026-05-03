c21184732dc33eab1e1f351bfc8b3cdc17fdc479
MO Othman <othman9128@gmail.com>
2026-05-03 11:24:12 +0300

    Implement the User details Service

--- Book-network/src/main/java/com/mohamed/Book_network/Security/JwtFilter.java
+++ Book-network/src/main/java/com/mohamed/Book_network/Security/JwtFilter.java
@@ -1,8 +1,48 @@
 package com.mohamed.Book_network.Security;
 
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-@Ser
+import java.io.IOException;
+import java.net.http.HttpHeaders;
 
-public class JwtFilter {
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Service
+
+@RequiredArgsConstructor
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final jwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    @NotNull FilterChain filterChain) throws ServletException, IOException {
+
+        if(request.getServletPath().contains("api/v1/auth")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        final String authHeader = request.getHeader(AUTHORIZATION);
+        final String jwt ;
+        final String userEmail ;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+
+        userEmail = jwtService.extractUsername(jwt);
+    }
 }
---
+++ Book-network/src/main/java/com/mohamed/Book_network/Security/jwtService.java
@@ -0,0 +1,8 @@
+package com.mohamed.Book_network.Security;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class jwtService {
+}
